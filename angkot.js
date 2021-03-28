074e476ad829cff2dc1b5c4045dd6070afa202ee
Honey-bee-code <hanifinida@gmail.com>
2021-03-28 11:37:48 +0800

    juragan angkot - hapus penumpang

--- angkot.js
+++ angkot.js
@@ -11,7 +11,7 @@ var naik = function (namaPenumpang) {
       return x === namaPenumpang;
     });
     if (kosong.length !== 0) {
-      penumpang.splice(1, 1, namaPenumpang);
+      penumpang.splice(1, 1, namaPenumpang); // harus pake for
       return penumpang;
     } else {
       if (cari.length == 0) {
@@ -19,6 +19,20 @@ var naik = function (namaPenumpang) {
         return penumpang;
       } else {
         console.log("Nama " + namaPenumpang + " sudah ada!");
+        return penumpang;
+      }
+    }
+  }
+};
+
+var turun = function (namaPenumpang) {
+  if (penumpang == 0) {
+    console.log("Tidak ada penumpang!");
+    return penumpang;
+  } else {
+    for (var i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[0] = undefined;
         return penumpang;
       }
     }
--- angkot2.js
+++ angkot2.js
@@ -15,10 +15,23 @@ var naik = function (namaPenumpang) {
         } else if (i == penumpang.length - 1) {
           penumpang.push(namaPenumpang);
           return penumpang;
-        } else if (penumpang[i] == namaPenumpang) {
-          console.log("Nama " + namaPenumpang + " sudah ada!");
-          return penumpang;
-        }
+        } 
+      }
+    }
+  }
+};
+var turun = function (namaPenumpang) {
+  if (penumpang == 0) {
+    console.log("Tidak ada penumpang!");
+    return penumpang;
+  } else {
+    for (var i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        console.log("Nama " + namaPenumpang + " tidak ada!");
+        return penumpang;
       }
     }
   }
