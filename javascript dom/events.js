1f423db1806006fa765154cea6a17e0b7985baff
Honey-bee-code <hanifinida@gmail.com>
2021-04-02 11:56:59 +0800

    DOM Events

--- javascript dom/events.js
+++ javascript dom/events.js
@@ -9,14 +9,14 @@
 // const p2 = document.querySelector(".p2");
 // p2.onclick = ubahWarnaP2;
 
-// const p4 = document.querySelector("section#b p");
-// p4.addEventListener("click", function () {
-//   const ul = document.querySelector("section#b ul");
-//   const liBaru = document.createElement("li");
-//   const teksLiBaru = document.createTextNode("item baru");
-//   liBaru.appendChild(teksLiBaru);
-//   ul.appendChild(liBaru);
-// });
+const p4 = document.querySelector("section#b p");
+p4.addEventListener("click", function () {
+  const ul = document.querySelector("section#b ul");
+  const liBaru = document.createElement("li");
+  const teksLiBaru = document.createTextNode("item baru");
+  liBaru.appendChild(teksLiBaru);
+  ul.appendChild(liBaru);
+});
 
 const p3 = document.querySelector(".p3");
 // p3.onclick = function () {
