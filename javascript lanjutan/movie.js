300d33ad4a49be759f401084916d72dc8bdfdbe0
Honey-bee-code <hanifinida@gmail.com>
2021-04-16 17:27:44 +0800

    movies database

--- javascript lanjutan/movie.js
+++ javascript lanjutan/movie.js
@@ -22,53 +22,69 @@
 //   },
 // });
 
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.status === 200) {
-    if (xhr.readyState === 4) {
-      const result = xhr.response;
-      const movies = JSON.parse(result).Search;
-      // console.log(movies);
-      let hasil = "";
-      movies.forEach((movie) => {
-        hasil += showHasil(movie);
-        document.querySelector(".movies").innerHTML = hasil;
-      });
-      const detailBtn = document.querySelectorAll(".modal-detail");
-      detailBtn.forEach(function (btn) {
-        btn.addEventListener("click", function () {
-          const id = btn.dataset.id;
-          let detail = "";
-          const xhrDetail = new XMLHttpRequest();
-          if (xhrDetail.status === 200) {
-            if (xhrDetail.readyState === 4) {
-              xhrDetail.onreadystatechange = function () {
-                const modalDetail = xhr.response.Search;
-                console.log(modalDetail)
-                detail += showDetail(modalDetail);
-                document.querySelector(".movie-modal").innerHTML = detail;
-              };
-            }
-          } else {
-            console.log(xhrDeta.response);
-          }
-          xhrDetail.open("get", "http://www.omdbapi.com/?apikey=a1e37a18&i=" + id);
-          xhrDetail.send();
-        });
-      });
-    }
-  } else {
-    console.log(xhr.response);
+const inputSearch = document.getElementById("input-search");
+const buttonSearch = document.getElementById("button-search");
+
+buttonSearch.addEventListener("click", function () {
+  movieDatabases();
+});
+inputSearch.addEventListener("keyup", function () {
+  if (event.keyCode === 13) {
+    movieDatabases();
   }
-};
-xhr.open("get", "http://www.omdbapi.com/?apikey=a1e37a18&s=avengers");
-xhr.send();
+});
+
+function movieDatabases() {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.status === 200) {
+      if (xhr.readyState === 4) {
+        const result = xhr.response;
+        const movies = JSON.parse(result).Search;
+        // console.log(movies);
+        let hasil = "";
+        movies.forEach((movie) => {
+          hasil += showHasil(movie);
+          document.querySelector(".movies").innerHTML = hasil;
+        });
+        const detailBtn = document.querySelectorAll(".modal-detail");
+        detailBtn.forEach(function (btn) {
+          btn.addEventListener("click", function () {
+            const id = btn.dataset.id;
+            // console.log(id);
+
+            xhr.onreadystatechange = function () {
+              if (xhr.status === 200) {
+                if (xhr.readyState === 4) {
+                  let detail = "";
+                  //   console.log(xhr.response);
+                  const modalDetail = JSON.parse(xhr.response);
+                  detail += showDetail(modalDetail);
+                  document.querySelector(".movie-modal").innerHTML = detail;
+                }
+              } else {
+                console.log(xhr.response);
+              }
+            };
+            xhr.open("get", "http://www.omdbapi.com/?apikey=a1e37a18&i=" + id);
+            xhr.send();
+          });
+        });
+      }
+    } else {
+      console.log(xhr.response);
+    }
+  };
+  xhr.open("get", "http://www.omdbapi.com/?apikey=a1e37a18&s=" + inputSearch.value);
+  xhr.send();
+}
 
 function showHasil(m) {
   return `
-    <div class="col-sm-2" >
+    <div class="col-sm-3" >
         <div class="card" >
         <div class="card-body">
+            <img src="${m.Poster}" width="100%">
             <h5 class="card-title">${m.Title}</h5>
             <p>${m.Year}</p>
             <button type="button" class="btn btn-primary modal-detail" data-bs-toggle="modal" data-bs-target="#movieModal" data-id="${m.imdbID}">
@@ -82,7 +98,7 @@ function showDetail(d) {
   return `
     <div class="row">
         <div class="col-md-3">
-            <img src="${d.Poster}" alt="">
+            <img src="${d.Poster}" width="100%">
         </div>
         <div class="col-md-9">
             <ul>
